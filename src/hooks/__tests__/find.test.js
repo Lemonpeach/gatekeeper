dd551a85c83e0e3f7436a82d2824f9679de3fd19
Peony Gerochi <peony@itsduc.com>
2019-02-22 11:12:05 -0500

    test: hooks/find test

---
+++ src/hooks/__tests__/find.test.js
@@ -0,0 +1,51 @@
+describe('hooks/find', () => {
+  beforeEach(() => {
+    jest.mock('engine', () => ({
+      createAuthHook: jest.fn()
+    }));
+  });
+
+  afterEach(() => {
+    jest.resetModules();
+  });
+
+  test('should delegate to find auth hooks', async () => {
+    const { find } = require('../find');
+    const options = {
+      authenticate: () => {},
+      ruleServiceName: 'authorization-rules',
+      getPrincipalId: () => {},
+      attributeLookup: {
+        principalId: () => {}
+      }
+    };
+
+    find(options);
+
+    const { createAuthHook } = require('engine');
+    const { getReadArguments, filterResponse } = require('utils');
+
+    expect(createAuthHook).toHaveBeenCalledWith({
+      ...options,
+      isAuthorized: filterResponse,
+      getArguments: getReadArguments,
+      onAuthorized: expect.any(Function)
+    });
+  });
+
+  test('should skip when authorized', () => {
+    const { find } = require('../find');
+    const { createAuthHook } = require('engine');
+    const { SKIP } = require('@feathersjs/feathers');
+    const options = {
+      authorized: true,
+      context: {}
+    };
+
+    find({});
+
+    const { onAuthorized } = createAuthHook.mock.calls[0][0];
+    expect(onAuthorized(options)).toBe(SKIP);
+    expect(options.context.result).toBe(true);
+  });
+});
